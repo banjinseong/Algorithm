fde3ff9be8e0bf6be34c11542b20f37154f52e82
banjinseong <88262512+banjinseong@users.noreply.github.com>
2025-03-28 11:56:10 +0900

    [Silver III] Title: 소수 구하기, Time: 496 ms, Memory: 23296 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,21 +1,33 @@
-import java.util.*;
-class Main {
-  public static void main(String[] args){
-    int count;
-    Scanner sc = new Scanner(System.in);
-    int a = sc.nextInt();
-    int b = sc.nextInt();
-    for(;a<=b; a++){
-      count = 0; 
-      for(int i=2; i*i<=a; i++){
-        if(a%i == 0){
-          count++;
-          break;
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] number = br.readLine().split(" ");
+        int begin = Integer.parseInt(number[0]);
+        int end = Integer.parseInt(number[1]);
+        int cnt=0;
+
+        for (int i = begin; i <= end; i++) {
+            if(i!=2 && i%2==0){
+                continue;
+            }
+            for(int j = 1; j <= Math.sqrt(i); j+=2) {
+                if(j!=1 && i%j==0){
+                    cnt++;
+                    break;
+                }
+            }
+            if(i!=1 && cnt==0){
+                bw.write(i+"\n");
+            }
+            cnt =0;
         }
-      }
-      if(count==0 && a != 1){
-        System.out.println(a);
-      }
+        bw.flush();
+        bw.close();
     }
-  }
 }
+
